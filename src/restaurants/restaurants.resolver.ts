795f161918a1d7146408c6157bf9f18a4abc5db7
k-sang-soo <wsckm1@gmail.com>
2023-12-19 23:43:12 +0900

    feat: arguments 전달 방법

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,13 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver()
 export class RestaurantsResolver {
   // Query가 return 하고자 하는 type을 return 하는 function이어여 한다.
-  @Query(() => Restaurant)
-  myRestaurant() {
-    return true;
+  // [Restaurant] 괄호 안에 넣는 방식이 GraphQL에서 배열을 표현하는 방법
+  @Query(() => [Restaurant])
+  // GraphQL에서 args 전달 방법
+  restaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
