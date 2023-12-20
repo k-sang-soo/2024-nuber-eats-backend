da70e169b62f7e6175580a53ae7bffe3e0e75cc5
k-sang-soo <wsckm1@gmail.com>
2023-12-20 23:47:47 +0900

    feat: validating ArgsTypes, useGlobalPipes 설정, dto 유효성 검사

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,4 +1,5 @@
 import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 /* dto 에는 InputType과 ArgsType 이 존재함
  * InputType : GraphQL 쿼리나 뮤테이션에서 단일 인자로 전달되는 객체를 정의하는데 사용
@@ -7,11 +8,16 @@ import { ArgsType, Field, InputType } from '@nestjs/graphql';
 @ArgsType()
 export class CreateRestaurantDto {
   @Field((type) => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
   @Field((type) => Boolean)
+  @IsBoolean()
   isVegan: boolean;
   @Field((type) => String)
+  @IsString()
   address: string;
   @Field((type) => String)
+  @IsString()
   ownerName: string;
 }
