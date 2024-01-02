4e09f64e0c91078de9745594fb111fa4439769f0
k-sang-soo <wsckm1@gmail.com>
2024-01-02 23:15:40 +0900

    feat: create Restaurat 설정, type 에러 fix 중

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,3 +1,4 @@
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Injectable } from '@nestjs/common';
 import { Restaurant } from './entities/restaurant.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -14,5 +15,13 @@ export class RestaurantService {
   getAll(): Promise<Restaurant[]> {
     // 이렇게 Repository를 접근할 수 있음
     return this.restaurants.find();
+  }
+  createRestaurant(
+    createRestaurantDto: CreateRestaurantDto,
+  ): Promise<Restaurant> {
+    // create는 새 엔티티 인스턴스를 만들고 이 개체의 모든 엔터티 속성을 새 엔티티에 복사. 엔터티 스키마에 있는 속성만 복사
+    // save는 엔티티를 데이터베이스에 저장 또는 업데이트, 데이터베이스에 지정된 모든 엔터티를 저장. 엔티티가 데이터베이스에 없으면 삽입하고, 그렇지 않으면 업데이트.
+    const newRestaurant = this.restaurants.create(createRestaurantDto);
+    return this.restaurants.save(newRestaurant);
   }
 }
