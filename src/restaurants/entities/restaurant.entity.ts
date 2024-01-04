5718fdd4c3b4e6bda9ca18f9105533d9076f8293
k-sang-soo <wsckm1@gmail.com>
2024-01-04 22:43:22 +0900

    feat; optional Types and Columns, entity에서 validtion 체크하는 방법

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,4 +1,5 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType() //자동으로 스키마를 빌드하기 위해 사용하는 GraphQL decorator
@@ -10,21 +11,17 @@ export class Restaurant {
 
   @Field((type) => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
-  @Field((type) => Boolean)
-  @Column()
+  @Field((type) => Boolean, { defaultValue: true })
+  @Column({ default: true })
+  @IsOptional() // value가 누락 됐는 지 확인하고, 없다면 모든 validator을 무시함. 해당 필드를 보내거나 보내지 않을 수 있음
+  @IsBoolean()
   isVegan?: boolean;
 
   @Field((type) => String)
   @Column()
   address: string;
-
-  @Field((type) => String)
-  @Column()
-  ownerName: string;
-
-  @Field((type) => String)
-  @Column()
-  categoryName: string;
 }
