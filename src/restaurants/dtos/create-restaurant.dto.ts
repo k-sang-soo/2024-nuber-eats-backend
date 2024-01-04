d2a449ca56afc6023c9e61daa88fdb7ff1d22bd2
k-sang-soo <wsckm1@gmail.com>
2024-01-04 22:26:36 +0900

    feat: mapped Types 설정

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,23 +1,14 @@
-import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { ArgsType, Field, InputType, OmitType } from '@nestjs/graphql';
 import { IsBoolean, IsString, Length } from 'class-validator';
+import { Restaurant } from '../entities/restaurant.entity';
 
 /* dto 에는 InputType과 ArgsType 이 존재함
  * InputType : GraphQL 쿼리나 뮤테이션에서 단일 인자로 전달되는 객체를 정의하는데 사용
  * ArgsType : 각각의 필드가 별도의 인자로 전달되는 객체를 정의하는 데 사용
  */
-@ArgsType()
-export class CreateRestaurantDto {
-  @Field((type) => String)
-  @IsString()
-  @Length(5, 10)
-  name: string;
-  @Field((type) => Boolean)
-  @IsBoolean()
-  isVegan: boolean;
-  @Field((type) => String)
-  @IsString()
-  address: string;
-  @Field((type) => String)
-  @IsString()
-  ownerName: string;
-}
+@InputType()
+export class CreateRestaurantDto extends OmitType(
+  Restaurant,
+  ['id'],
+  InputType,
+) {}
