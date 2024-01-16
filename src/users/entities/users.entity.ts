01d9551ca9ba364bb9af2e76524174451135f99a
k-sang-soo <wsckm1@gmail.com>
2024-01-16 22:33:38 +0900

    chore: 로직 흐름 주석 처리 추가

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -19,7 +19,12 @@ enum UserRole {
 //GraphQL에서 enum type 사용 방법
 registerEnumType(UserRole, { name: 'UserRole' });
 
+// InputType 는 입력 값(@Args)을 "User" 라는 입력 타입으로 받겠다는 의미
+//isAbstract 는 GraphQL 스키마에 직접적으로 나타나지 않지만
+// "User" 클래스가 "CoreEntity" 필드들을 상속 받아 사용
+// 이렇게 하는 이유는 중복되는 설정을 관리하고 확장성 있는 구조를 만들기 위해 사용
 @InputType({ isAbstract: true })
+// ObjectType 은 reutrn 값을 "User"로 받겠다는 의미
 @ObjectType()
 @Entity()
 export class User extends CoreEntity {
