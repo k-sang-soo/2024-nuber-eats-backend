426cfff502bd0d1f7ac054523e91718728338f6a
k-sang-soo <wsckm1@gmail.com>
2024-01-20 23:42:08 +0900

    feat: verifying user 접근하는 로직만 설정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -129,4 +129,17 @@ export class UsersService {
 
     return this.users.save(user);
   }
+
+  async verifyEmail(code: string): Promise<boolean> {
+    const verification = await this.verification.findOne({
+      where: { code },
+      relations: ['user'],
+    });
+    if (verification) {
+      verification.user.verified = true;
+      this.users.save(verification.user);
+    }
+
+    return false;
+  }
 }
